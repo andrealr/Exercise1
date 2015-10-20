bbc96c4bcd28b5368f0ad3f2de5fc4f3b7e8d9e6
andrea rongved <andrealr@stud.ntnu.no>
2015-10-20 08:55:41 +0200

    Creating helloUser

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,12 +7,15 @@
  *
  */
 public class HelloWorld {
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		HelloUser name = new HelloUser();
+		name.greetUser();
 	}
 
 }
